1e7fa2465880211a57a682f8f490b4fd92d840e6
paula hage <hage.paula@hotmail.com>
2023-04-05 17:08:48 +0200

    #12 Added the side navbar button

--- src/components/sideNavbar/SideNavbar.tsx
+++ src/components/sideNavbar/SideNavbar.tsx
@@ -1,10 +1,12 @@
-import { Container, Box } from '@mui/material'
-import React from 'react'
+import { Box, IconButton } from "@mui/material";
+import React from "react";
 
 import "./SideNavbar.scss";
 
 export function SideNavbar() {
-  return (
-    <Box className='sideNavbar'>SideNavbar</Box>
-  )
+  return <Box className="sideNavbar">
+    <IconButton>
+      <img src="../icons/box_icon.svg" alt="my box button" className="sideNavbar__btn" />
+    </IconButton>
+  </Box>;
 }
