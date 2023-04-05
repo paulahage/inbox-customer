9b5297c9faf48f9a2288f3f5872020a4be48aa0b
paula hage <hage.paula@hotmail.com>
2023-04-05 18:17:16 +0200

    #25 Added typing loading when customer is typing

--- src/components/chatBox/ChatNavbar.tsx
+++ src/components/chatBox/ChatNavbar.tsx
@@ -1,7 +1,7 @@
 import "./ChatNavbar.scss";
 import Customer from "../customer/Customer";
 import CustomerName from "../customer/CustomerName";
-import { Stack, Button } from "@mui/material";
+import { Stack, Button, Typography } from "@mui/material";
 import { Ticket } from "../../ApiModels";
 interface Props {
   ticket: Ticket;
@@ -12,17 +12,31 @@ export default function ChatNavbar({ ticket }: Props) {
     <Stack
       direction="row"
       justifyContent="space-between"
-      alignItems="center"
+      alignItems="flex-start"
       sx={{ width: "100%", height: "58px" }}
+      spacing={2}
     >
       <Stack
         direction="row"
-        spacing={1}
+        spacing={2}
         alignItems="center"
         paddingLeft={"15px"}
       >
         <Customer customer={ticket.customer} />
-        <CustomerName customer={ticket.customer} />
+        <Stack direction="column" spacing={1} alignItems="flex-start">
+          <CustomerName customer={ticket.customer} />
+          <Stack direction="row" alignItems="flex-end" spacing={0.3}>
+            <Typography
+              variant="subtitle2"
+              fontWeight={"light"}
+              fontSize={13}
+              className="chatNavbar__typing"
+            >
+              typing
+            </Typography>
+            <img src="../icons/typing.gif" alt="customer typing" className="chatNavbar__loading_typing"/>
+          </Stack>
+        </Stack>
       </Stack>
       <Stack
         direction="row"
@@ -35,6 +49,7 @@ export default function ChatNavbar({ ticket }: Props) {
           size="medium"
           color="secondary"
           sx={{ borderRadius: 5, fontSize: 12, textTransform: "none" }}
+          className="chatNavbar__re-assign-btn"
         >
           Re-Assign
         </Button>
